66c847364fe65e92e0b514f9b4fb518267ab0b70
xinil502 <1223176327@qq.com>
2020-05-08 11:28:14 +0800

    LeetCode_221.寻找旋转排序数组中的最小值。

---
+++ src/leetcode/LeetCode_153.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class LeetCode_153 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for(int i=0; i<n; ++i){
+            nums[i] = sc.nextInt();
+        }
+
+        Solution_153 sol = new Solution_153();
+        System.out.println(sol.findMin(nums));
+    }
+}
+
+class Solution_153 {
+    public int findMin(int[] nums) {
+        int l=0, r=nums.length-1;
+        while(l<r){
+            if(nums[(l+r)/2] <= nums[r]){
+                r = (l+r)/2;
+            }else{
+                l = (l+r)/2+1;
+            }
+        }
+        return nums[r];
+    }
+}
