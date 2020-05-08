982ee12ecec363828473e4b3d7eed4d56c8178cb
xinil502 <1223176327@qq.com>
2020-05-08 10:16:05 +0800

    LeetCode_221.最大正方形

--- src/leetcode/LeetCode_221.java
+++ src/leetcode/LeetCode_221.java
@@ -21,22 +21,22 @@ public class LeetCode_221 {
 class Solution_221 {
     public int maximalSquare(char[][] matrix) {
         int m = matrix.length , n;
-        if(m == 0){
+        if(m == 0){  //数组为空直接返回0。
             return 0;
         }else{
             n = matrix[0].length;
         }
         int max = 0;
-        int[][] dp =new int[m][n];
+        int[][] dp =new int[m][n];   //以该位置为右下角的最大正方形的边长。
         for(int i = 0; i<m; ++i){
             for(int j = 0; j<n; ++j){
-                if(matrix[i][j] == '0'){
+                if(matrix[i][j] == '0'){  //字符为0，没有正方形
                     dp[i][j] = 0;
                 }else{
-                    if(i==0 || j==0){
+                    if(i==0 || j==0){  //字符为1 且位于边，正方形最大为1.
                         dp[i][j] = 1;
                     }else{
-                        dp[i][j] = min(dp[i-1][j-1],dp[i][j-1],dp[i-1][j])+1;
+                        dp[i][j] = min(dp[i-1][j-1],dp[i][j-1],dp[i-1][j])+1;  //不位于边上，能扩展的最大正方形为上，左，左上三个方向的最小正方形边长+1.
                     }
                 }
             }
