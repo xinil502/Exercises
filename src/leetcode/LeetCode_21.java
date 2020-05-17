219fcb5263f5a42c0b1c976a832f13761a11d7eb
xinil502 <1223176327@qq.com>
2020-05-17 18:45:22 +0800

    LeetCode_21.合并两个有序链表

--- src/leetcode/LeetCode_21.java
+++ src/leetcode/LeetCode_21.java
@@ -8,23 +8,10 @@ import java.util.Scanner;
 public class LeetCode_21 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int m, n;
-        m = sc.nextInt();
-        n = sc.nextInt();
-
-        ListNode_21 l1 = new ListNode_21(sc.nextInt()), x;
-        x = l1;
-        for(int i=1; i<m; ++i){
-            x.next = new ListNode_21(sc.nextInt());
-            x = x.next;
-        }
-
-        ListNode_21 l2 = new ListNode_21(sc.nextInt());
-        x = l2;
-        for(int i=1; i<n; ++i){
-            x.next = new ListNode_21(sc.nextInt());
-            x = x.next;
-        }
+        ListNode l1 = new ListNode(sc.nextInt());
+        l1.BuildList();
+        ListNode l2 = new ListNode(sc.nextInt());
+        l2.BuildList();
 
         Solution_21 sol = new Solution_21();
         l1 = sol.mergeTwoLists(l1, l2);
@@ -36,18 +23,18 @@ public class LeetCode_21 {
 }
 
 class Solution_21 {
-    public ListNode_21 mergeTwoLists(ListNode_21 l1, ListNode_21 l2) {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
         if(l1 == null){ //若有一个链表为空，则返回另一个链表。
             return l2;
         }else if(l2 == null){
             return  l1;
         }
-        ListNode_21 list, head;  //创建链表头结点
+        ListNode list, head;  //创建链表头结点
         if(l1.val<l2.val){
-            head = list = new ListNode_21(l1.val);
+            head = list = new ListNode(l1.val);
             l1 = l1.next;
         }else {
-            head = list = new ListNode_21(l2.val);
+            head = list = new ListNode(l2.val);
             l2 = l2.next;
         }
         while(true){  //有一个链表为空时，，将另一个链表接上并返回。
@@ -70,13 +57,5 @@ class Solution_21 {
             }
         }
         return head;
-    }
-}
-class ListNode_21{
-    int val;
-    ListNode_21 next;
-    ListNode_21(int x){
-        val = x;
-        next = null;
     }
 }
