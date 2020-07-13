0f0a9bc4735cc9cbb0364a4af9fed582e97b7cfc
xinil502 <1223176327@qq.com>
2020-07-13 12:14:47 +0800

    LeetCode_76.最小覆盖子串

--- src/leetcode/LeetCode_76.java
+++ src/leetcode/LeetCode_76.java
@@ -1,6 +1,6 @@
 package leetcode;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class LeetCode_76 {
     public static void main(String[] args) {
@@ -20,45 +20,56 @@ class Solution_76 {
             return "";
         }
         String min = s;
-        int i, j, k, judge = 0;
-        int[] num = new int[t.length()];
-        int[] numm = new int[t.length()];
-        for (k = 0; k < t.length(); ++k) {
-            for (i = 0; i < t.length(); ++i) {
-                if (t.charAt(i) == t.charAt(k)) {
-                    ++numm[k];
-                }
+        int i, j, judge = 0;
+        Map<Character, Integer> numWindow = new HashMap<>(); //初始化统计哈希表
+        Map<Character, Integer> numT = new HashMap<>();//初始化参照哈希表
+        for (i = 0; i < t.length(); ++i) {
+            if (numT.get(t.charAt(i)) == null) {
+                numT.put(t.charAt(i), 1);
+            } else {
+                numT.put(t.charAt(i), numT.get(t.charAt(i)) + 1);
             }
         }
+        Set<Character> set = numT.keySet();
+
         for (i = 0, j = 0; j < s.length(); ++j) {//右端右移
-            for (k = 0; k < t.length(); ++k) {  //判断是否为t串中的字符
-                if (s.charAt(j) == t.charAt(k)) {
-                    ++num[k];
-                }
+            // 加入右端新字符
+            if (numWindow.get(s.charAt(j)) == null) {
+                numWindow.put(s.charAt(j), 1);
+            } else {
+                numWindow.put(s.charAt(j), numWindow.get(s.charAt(j)) + 1);
             }
-            for (k = 0; k < t.length(); ++k) { //判断是否已经覆盖字符串t
-                if (num[k] < numm[k]) {
+
+            //判断是否满足覆盖
+            int fuGai = 1;
+            for (Iterator<Character> iter = set.iterator(); iter.hasNext(); ) {
+                char temp = iter.next();
+                if (numWindow.get(temp) == null || numT.get(temp) > numWindow.get(temp)) { //不满足，右端继续右移
+                    fuGai = 0;
                     break;
                 }
             }
-            if (k != t.length()) { //未覆盖，，++j， 右端右移，进行下一次循环
+            if (fuGai == 0) {
                 continue;
             }
-            judge = 0;
-            for (; judge == 0; ++i) { //尝试左端右移
-                String kk = s.substring(i, j + 1);
-                if (kk.length() < min.length()) {
-                    min = kk; //已经覆盖，，赋值
+
+            //满足，左端左移至最小子串
+            char ch = s.charAt(i);
+            while (numT.get(ch) == null || numT.get(ch) <= numWindow.get(ch)) {
+                numWindow.put(ch, numWindow.get(ch) - 1);
+                ++i;
+                if (numT.get(ch) != null && numT.get(ch) > numWindow.get(ch)) {
+                    break;
                 }
-                for (k = 0; k < t.length(); ++k) {
-                    if (s.charAt(i) == t.charAt(k)) {
-                        --num[k];
-                        if (num[k] < numm[k]) {  //如果右移后不存在于子串中，跳出
-                            judge = 1;
-                        }
-                    }
-                }
+                ch = s.charAt(i);
             }
+
+            String kk = s.substring(i - 1, j + 1);
+            if (kk.length() <= min.length()) {
+                min = kk; //已经覆盖，，赋值
+                judge = 1;
+            }
+
         }
         if (judge == 1) { //覆盖过
             return min;
