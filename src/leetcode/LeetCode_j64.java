e9d06cecd336ac53716c3f44462d306b5e5c4eec
xinil502 <1223176327@qq.com>
2020-07-21 18:23:53 +0800

    剑指Offer 64.求1+2+...n

--- src/leetcode/LeetCode_j64.java
+++ src/leetcode/LeetCode_j64.java
@@ -2,17 +2,17 @@ package leetcode;
 
 import java.util.Scanner;
 
-public class LeetCode_m64 {
+public class LeetCode_j64 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        Solution_m64 sol = new Solution_m64();
+        Solution_j64 sol = new Solution_j64();
         System.out.println(sol.sumNums(n));
     }
 }
 
-class Solution_m64 { //用短路运算代替if判断语句，，递归求解。
+class Solution_j64 { //用短路运算代替if判断语句，，递归求解。
     public int sumNums(int n) {
         boolean flag = n > 1 && (n += this.sumNums(n - 1)) == 0;
         return n;
