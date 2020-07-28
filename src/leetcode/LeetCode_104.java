cd5e74b7ec21a8ee15a41c4511c0d5c4d1e0afc1
xinil502 <1223176327@qq.com>
2020-07-28 12:06:18 +0800

    LeetCode_104.二叉树的最大深度

---
+++ src/leetcode/LeetCode_104.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+import java.util.Scanner;
+
+/**
+ * LeetCode_104.二叉树的最大深度
+ */
+public class LeetCode_104 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        TreeNode root = new TreeNode(sc.nextInt());
+        root.CreatTree();
+
+        Solution_104 sol = new Solution_104();
+        System.out.println(sol.maxDepth(root));
+    }
+}
+
+class Solution_104 {
+    public int maxDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        } else {
+            return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
+        }
+    }
+}
