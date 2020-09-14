3f89c920b6a89981632af97e7689b36c127bad15
xinil502 <1223176327@qq.com>
2020-09-14 22:57:44 +0800

    LeetCode_94.二叉树的中序遍历

---
+++ src/leetcode/LeetCode_94.java
@@ -0,0 +1,42 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
+
+public class LeetCode_94 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        TreeNode root = new TreeNode(sc.nextInt());
+        root.CreatTree();
+
+        Solution_94 sol = new Solution_94();
+        List<Integer> list = sol.inorderTraversal(root);
+        for (Iterator<Integer> ite = list.iterator(); ite.hasNext(); ) {
+            System.out.println(ite.next());
+        }
+    }
+}
+
+class Solution_94 {
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        dfs(root);
+        return list;
+    }
+
+    void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        if (root.left != null) {
+            dfs(root.left);
+        }
+        list.add(root.val);
+        if (root.right != null) {
+            dfs(root.right);
+        }
+    }
+}
