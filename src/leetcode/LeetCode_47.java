c8a95b67810acd84a5f63adad58168e7a43e1389
xinil502 <1223176327@qq.com>
2020-09-19 00:01:23 +0800

    LeetCode_47.全排列Ⅱ

---
+++ src/leetcode/LeetCode_47.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+import java.util.*;
+
+public class LeetCode_47 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; ++i) {
+            nums[i] = sc.nextInt();
+        }
+
+        Solution_47 sol = new Solution_47();
+        for (Iterator<List<Integer>> ite1 = sol.permuteUnique(nums).iterator(); ite1.hasNext(); ) {
+            for (Iterator<Integer> ite2 = ite1.next().iterator(); ite2.hasNext(); ) {
+                System.out.print(ite2.next());
+            }
+            System.out.println();
+        }
+    }
+}
+
+class Solution_47 {
+    boolean[] vis;
+
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> perm = new ArrayList<Integer>();
+        vis = new boolean[nums.length];
+        Arrays.sort(nums);
+        backtrack(nums, ans, 0, perm);
+        return ans;
+    }
+
+    public void backtrack(int[] nums, List<List<Integer>> ans, int idx, List<Integer> perm) {
+        if (idx == nums.length) {
+            ans.add(new ArrayList<Integer>(perm));
+            return;
+        }
+        for (int i = 0; i < nums.length; ++i) {
+            if (vis[i] || (i > 0 && nums[i] == nums[i - 1] && !vis[i - 1])) {
+                continue;
+            }
+            perm.add(nums[i]);
+            vis[i] = true;
+            backtrack(nums, ans, idx + 1, perm);
+            vis[i] = false;
+            perm.remove(idx);
+        }
+    }
+}
