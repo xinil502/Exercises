bcd058bdced5665b919c07506605903a3ef73686
xinil502 <1223176327@qq.com>
2020-09-19 14:17:49 +0800

    TreeNode creatTree方法名称修改

---
+++ src/leetcode/LeetCode_938.java
@@ -0,0 +1,52 @@
+package leetcode;
+
+import java.util.Scanner;
+
+/**
+ * LeetCode_938.二叉搜索树的范围和
+ */
+public class LeetCode_938 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        TreeNode root = new TreeNode(sc.nextInt());
+        root.creatTree();
+
+        Solution_938 sol = new Solution_938();
+        System.out.println(sol.rangeSumBST(root, sc.nextInt(), sc.nextInt()));
+    }
+}
+
+class Solution_938 {
+    int sum;
+    int l, r;
+
+    public int rangeSumBST(TreeNode root, int L, int R) {
+        sum = 0;
+        l = L;
+        r = R;
+        dfs(root);
+        return sum;
+    }
+
+    void dfs(TreeNode root) {
+        if (null == root) {
+            return;
+        }
+        if (root.val <= l) {
+            if (root.val == l) {
+                sum += l;
+            }
+            dfs(root.right);
+            return;
+        } else if (root.val >= r) {
+            if (root.val == r) {
+                sum += r;
+            }
+            dfs(root.left);
+            return;
+        }
+        sum += root.val;
+        dfs(root.right);
+        dfs(root.left);
+    }
+}
