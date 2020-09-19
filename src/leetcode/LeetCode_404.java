f6d7eb587730fa6b760eb9913ac48c287bf6d5a0
xinil502 <1223176327@qq.com>
2020-09-19 13:49:56 +0800

    LeetCode_404.左叶子之和

---
+++ src/leetcode/LeetCode_404.java
@@ -0,0 +1,38 @@
+package leetcode;
+
+import java.util.Scanner;
+
+/**
+ * LeetCode_404.左叶子之和
+ */
+public class LeetCode_404 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        TreeNode root = new TreeNode(sc.nextInt());
+        root.creatTree();
+
+        Solution_404 sol = new Solution_404();
+        System.out.println(sol.sumOfLeftLeaves(root));
+    }
+}
+
+class Solution_404 {
+    int n;
+
+    public int sumOfLeftLeaves(TreeNode root) {
+        n = 0;
+        dfs(root);
+        return n;
+    }
+
+    void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        if (root.left != null && root.left.left == null && root.left.right == null) {
+            n += root.left.val;
+        }
+        dfs(root.right);
+        dfs(root.left);
+    }
+}
