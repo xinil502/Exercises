a09caa9dace8c9fef83333997b533827fcbddbae
xinil502 <1223176327@qq.com>
2020-09-21 16:35:26 +0800

    LeetCoe_538.将二叉搜索树改为二叉累加树

---
+++ src/leetcode/LeetCoe_538.java
@@ -0,0 +1,33 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class LeetCoe_538 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        TreeNode root = new TreeNode(sc.nextInt());
+        root.creatTree();
+
+        Solution_538 sol = new Solution_538();
+        sol.convertBST(root).print(1);
+    }
+}
+
+class Solution_538 {
+    int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        dfs(root);
+        return root;
+    }
+
+    void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        convertBST(root.right);
+        root.val += sum;
+        sum = root.val;
+        convertBST(root.left);
+    }
+}
