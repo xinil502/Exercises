39fe758568e33249a5f7b1045854bde9e7fe242d
xinil502 <1223176327@qq.com>
2020-09-25 19:03:35 +0800

    LeetCode_50.Pow(x,n)

--- src/leetcode/LeetCode_50.java
+++ src/leetcode/LeetCode_50.java
@@ -14,19 +14,19 @@ public class LeetCode_50 {
 }
 
 class Solution_50_1 {
-    public double myPow(double x, int n) {//将n转换为二进制求出下x^10 = x^(2^0+2^3) = x * x^9
-        long nn = n;
-        double num = 1;
-        double xn = x;
+    public double myPow(double x, int n) {//将n转换为二进制求出下x^10 = x^(2^1+2^3) = x^2 * x^8
+        long nn = n;  //避免负数求绝对值时越界的情况
+        double num = 1;  //累乘结果
+        double xn = x;   //计算 X^(2^n)
         if(nn < 0){
             nn = -nn;
             xn = 1 / x;
         }
         while(nn > 0){
-            if(nn%2 == 1){
+            if (nn % 2 == 1) { //该位有值，将Xn乘进去。
                 num *= xn;
             }
-            xn *= xn;  //右移计算下一位
+            xn *= xn;  //右移计算下一个X^(2^n)
             nn /= 2;
         }
         return num;
@@ -35,20 +35,18 @@ class Solution_50_1 {
 
 class Solution_50_2 {  //递归总是超时
     public double myPow(double x, int n) {
-        if(n == 1){
+        if (n == 0) {
+            return 1;
+        } else if (n == 1) {
             return x;
-        }
-        if(n == -1){
+        } else if (n == -1) {
             return 1/x;
-        }
-        if(n%2 == 0){
+        } else if (n % 2 == 0) {
             return myPow(x, n/2) * myPow(x, n/2);
-        }else {
-            if (n > 0) {
-                return myPow(x, n / 2) * myPow(x, n / 2) * x;
-            }else{
-                return myPow(x, n / 2) * myPow(x, n / 2) / x;
-            }
+        } else if (n > 0) {
+            return myPow(x, n / 2) * myPow(x, n / 2) * x;
+        } else {
+            return myPow(x, n / 2) * myPow(x, n / 2) / x;
         }
     }
 }
