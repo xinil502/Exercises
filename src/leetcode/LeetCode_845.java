75374dffab57c8f226d9b2374a5c9f24ff555be7
xinil502 <1223176327@qq.com>
2020-10-25 14:22:26 +0800

    845. 数组中的最长山脉

---
+++ src/leetcode/LeetCode_845.java
@@ -0,0 +1,45 @@
+package leetcode;
+
+import java.util.Scanner;
+
+/**
+ * 845. 数组中的最长山脉
+ */
+public class LeetCode_845 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; ++i) {
+            nums[i] = scanner.nextInt();
+        }
+
+        Solution_845 sol = new Solution_845();
+        System.out.println(sol.longestMountain(nums));
+    }
+}
+
+class Solution_845 {
+    public int longestMountain(int[] A) {
+        int n = A.length;
+        if (n == 0) {
+            return 0;
+        }
+        int[] left = new int[n];
+        for (int i = 1; i < n; ++i) {
+            left[i] = A[i - 1] < A[i] ? left[i - 1] + 1 : 0;
+        }
+        int[] right = new int[n];
+        for (int i = n - 2; i >= 0; --i) {
+            right[i] = A[i + 1] < A[i] ? right[i + 1] + 1 : 0;
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            if (left[i] > 0 && right[i] > 0) {
+                ans = Math.max(ans, left[i] + right[i] + 1);
+            }
+        }
+        return ans;
+    }
+}
