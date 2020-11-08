15bd8c8655f99a188f25afcb4440c790c08b97bb
xinil502 <1223176327@qq.com>
2020-11-08 13:09:29 +0800

    工具类，获取定长数组输入

---
+++ src/leetcode/Utils.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.Scanner;
+
+public class Utils {
+    static int[] createArray() {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+        for (int i = 0; i < n; ++i) {
+            nums[i] = scanner.nextInt();
+        }
+        scanner.close();
+        return nums;
+    }
+}
