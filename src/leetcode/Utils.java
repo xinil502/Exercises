1ec9f6255e709b6fcff304bfe21838b92a40fe5e
xinil502 <1223176327@qq.com>
2020-11-16 17:24:57 +0800

    工具类：创建并初始化二维数组

--- src/leetcode/Utils.java
+++ src/leetcode/Utils.java
@@ -3,7 +3,7 @@ package leetcode;
 import java.util.Scanner;
 
 public class Utils {
-    static int[] createArray() {
+    static int[] createArray() {   //创建并初始化一维数组
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         int[] nums = new int[n];
@@ -13,7 +13,21 @@ public class Utils {
         return nums;
     }
 
-    static ListNode createList() {
+    static ListNode createList() {  //创建并初始化链表
         return ListNode.createList();
+    }
+
+    static int[][] createDoubleDimensionalArray() {  //创建并初始化二维数组
+        Scanner scanner = new Scanner(System.in);
+        int m, n;
+        m = scanner.nextInt();
+        n = scanner.nextInt();
+        int[][] nums = new int[m][n];
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                nums[i][j] = scanner.nextInt();
+            }
+        }
+        return nums;
     }
 }
