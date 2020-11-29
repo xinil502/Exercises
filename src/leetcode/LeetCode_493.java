eee0d5cfbd05f7bdd0ca6bc94d205e105dc615b2
xinil502 <1223176327@qq.com>
2020-11-29 13:05:56 +0800

    164. 最大间距

--- src/leetcode/LeetCode_493.java
+++ src/leetcode/LeetCode_493.java
@@ -9,6 +9,10 @@ public class LeetCode_493 {
     }
 }
 
+/**
+ * 归并排序思路
+ * 数组的翻转对 = 左反转对 + 右翻转对 + 一左一右
+ */
 class Solution_693 {
     int[] val = null;
 
@@ -21,8 +25,8 @@ class Solution_693 {
         return re[0];
     }
 
-    int[] reverse(int start, int end) {
-        if (end - start < 2) {
+    int[] reverse(int start, int end) { // 计算翻转对的方法，第一个位置存储翻转对数目，之后的位置存储排序子数组。
+        if (end - start < 2) {   //长度为0或1，递归结束直接返回。
             if (end == start) {
                 return new int[]{0};
             }
@@ -31,24 +35,30 @@ class Solution_693 {
             re[1] = val[start];
             return re;
         }
-        int middle = (start + end) / 2;
-        int[] left, right;
+        //求出中位索引值（防止索引值太大相加超过int）。
+        int middle = start / 2 + end / 2;
+        if (start % 2 == 1 && end % 2 == 1) {
+            ++middle;
+        }
+        int[] left, right;  //接收递归后的左右子数组。
         left = reverse(start, middle);
         right = reverse(middle, end);
         int[] re = new int[left.length + right.length - 1];
         re[0] = left[0] + right[0];
         int i = 1, j = 1;
-        while (true) {
-            if (i == left.length) {
+        while (true) { //求左一右一的翻转对。
+            if (i == left.length) { //大的那边遍历完了，跳出循环
                 break;
             }
-            if (j != right.length && left[i] > 2 * right[j]) {
+            //右边没遍历完 &&（可能还有更多右边满足要求）：右侧索引值自增
+            if (j != right.length && (left[i] / 2 > right[j] || left[i] / 2 == right[j] && left[i] % 2 == 1)) {
                 ++j;
-            } else {
+            } else { //对于一个左侧值，统计有多少右侧值满足要求，加起来
                 re[0] += j - 1;
                 ++i;
             }
         }
+        //子数组合并
         int m = 1, n = 1;
         for (i = 1; m < left.length && n < right.length; ++i) {
             if (left[m] < right[n]) {
