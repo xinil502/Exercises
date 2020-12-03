f1e6088641728ce3fe1ca39231499237a5fcca69
xinil502 <1223176327@qq.com>
2020-12-03 18:11:48 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置

---
+++ src/leetcode/LeetCode_204.java
@@ -0,0 +1,25 @@
+package leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 204. 计数质数
+ */
+public class LeetCode_204 {
+    class Solution {
+        public int countPrimes(int n) {
+            int sum = 0;
+            boolean[] isPrime = new boolean[n];
+            Arrays.fill(isPrime, true);
+            for (int i = 2; i < n; ++i) {
+                if (isPrime[i]) {
+                    ++sum;
+                    for (int j = 2 * i; j < n; j += i) {
+                        isPrime[j] = false;
+                    }
+                }
+            }
+            return sum;
+        }
+    }
+}
