0cb9d78232d51484e6df6729fcd9cacde88162a9
xinil502 <1223176327@qq.com>
2021-01-02 09:32:54 +0800

    239.滑动窗口的最大值

---
+++ src/leetcode/LeetCode_239.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+import java.util.PriorityQueue;
+
+/**
+ * 239.滑动窗口的最大值
+ */
+public class LeetCode_239 {
+    class Solution {
+        public int[] maxSlidingWindow(int[] nums, int k) {
+            int n = nums.length;
+            PriorityQueue<int[]> pq = new PriorityQueue<>((pair1, pair2) -> pair1[0] != pair2[0] ? pair2[0] - pair1[0] : pair2[1] - pair1[1]);
+            for (int i = 0; i < k; ++i) {
+                pq.offer(new int[]{nums[i], i});
+            }
+            int[] ans = new int[n - k + 1];
+            ans[0] = pq.peek()[0];
+            for (int i = k; i < n; ++i) {
+                pq.offer(new int[]{nums[i], i});
+                while (pq.peek()[1] <= i - k) {
+                    pq.poll();
+                }
+                ans[i - k + 1] = pq.peek()[0];
+            }
+            return ans;
+        }
+    }
+}
