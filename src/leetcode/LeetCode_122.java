99c850f29924f7c9a92eda7f3af3096e57958301
xinil502 <1223176327@qq.com>
2021-06-14 18:01:10 +0800

    122.买卖股票的最佳时机Ⅱ

--- src/leetcode/LeetCode_122.java
+++ src/leetcode/LeetCode_122.java
@@ -4,28 +4,24 @@ package leetcode;
  * 122.买卖股票的最佳时机Ⅱ
  */
 public class LeetCode_122 {
-    public static void main(String[] args) {
-        Solution_122 sol = new Solution_122();
-        System.out.println(sol.maxProfit(Utils.createArray()));
-    }
-}
-
-class Solution_122 {
-    public int maxProfit(int[] prices) {
-        if (prices == null || prices.length < 2) {
-            return 0;
-        }
-        int sum = 0, start = 0;
-        for (int i = 1; i < prices.length; ++i) {
-            if (start == i - 1 && prices[i] < prices[i - 1]) {
-                start = i;
-            } else if (start != i - 1 && prices[i] < prices[i - 1]) {
-                sum += prices[i - 1] - prices[start];
-                start = i;
-            } else if (i == prices.length - 1 && prices[i] > prices[start]) {
-                sum += prices[i] - prices[start];
+    class Solution {
+        public int maxProfit(int[] prices) {
+            if (prices == null || prices.length < 2)
+                return 0;
+            int total = 0, index = 0, length = prices.length;
+            while (index < length) {
+                //如果股票下跌就一直找，直到找到股票开始上涨为止
+                while (index < length - 1 && prices[index] >= prices[index + 1])
+                    index++;
+                //股票上涨开始的值，也就是这段时间上涨的最小值
+                int min = prices[index];
+                //一直找到股票上涨的最大值为止
+                while (index < length - 1 && prices[index] <= prices[index + 1])
+                    index++;
+                //计算这段上涨时间的差值，然后累加
+                total += prices[index++] - min;
             }
+            return total;
         }
-        return sum;
     }
 }
